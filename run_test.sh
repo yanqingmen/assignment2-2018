7c0a5858efee8980c5fcc39363e10e29c6a40952
yanqingmen <toofooltosay@gmail.com>
2018-06-03 04:38:46 -0700

    change test script

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,3 @@
-export PYTHONPATH="${PYTHONPATH}:/home/hzx/git/yanqingmen/assignment2-2018/python"
-nosetests-3.4 -v tests/test_tvm_op.py
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+export PYTHONPATH="${PYTHONPATH}:${SHELL_FOLDER}/python"
+nosetests -v tests/test_tvm_op.py
